630c9e451c81e1d3d1c8f71fda30bfe0b02ae9fd
VairosX Dz <61067276+MoussaUfonet@users.noreply.github.com>
2020-03-19 13:00:10 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+clear
+echo """
+		 ____________________________________________
+		|					                         |
+		|          The Code By :- C.L.A.Y.H.A.K.     
+		|               twitter:- @ShadowDz5
+		|____________________________________________| """
+pkg install figlet -y 
+cp -a C.L.A.Y.py /data/data/com.termux/files/usr/bin
+echo "run [C.L.A.Y.py] in now tabe "
